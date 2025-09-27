61e75beafa644a29db2a7902069f0d093c413956
hgulizoda <hgulizodait@gmail.com>
2025-09-27 16:16:37 +0500

    scroll corrected

--- script.js
+++ script.js
@@ -21,15 +21,19 @@ const modal = document.getElementById("modal-box");
 const closeModal = document.getElementById("close-modal");
 const modalBtn = document.getElementById("subscribe");
 modalBtn.style.cursor = "pointer";
+const body = document.getElementById("body");
+body.style.height = "100vh";
 
 modalBtn.addEventListener("click", () => {
   modalOverlay.classList.add("openedY");
   modal.classList.add("openedY");
+  body.style.overflow = "hidden";
 });
 
 closeModal.addEventListener("click", () => {
   modalOverlay.classList.remove("openedY");
   modal.classList.remove("openedY");
+  body.style.overflowY = "visible";
 });
 
 const drawerOverlay = document.getElementById("drawer-overlay");
@@ -42,9 +46,11 @@ closeDrawer.style.cursor = "pointer";
 drawerBtn.addEventListener("click", () => {
   drawerOverlay.classList.add("openedX");
   drawer.classList.add("openedX");
+  body.style.overflow = "hidden";
 });
 
 closeDrawer.addEventListener("click", () => {
   drawerOverlay.classList.remove("openedX");
   drawer.classList.remove("openedX");
+  body.style.overflowY = "visible";
 });
